be15ccf335ce181b20ba79465e9234763393cc79
gustavo falcione <falcionegustavo@gmail.com>
2022-06-26 15:20:11 -0300

    Criando pagina de SignIn e componentizando input

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,30 @@
-import { Flex } from '@chakra-ui/react'
+import { Flex, Button, Stack } from '@chakra-ui/react'
+import { Input } from '../components/Form/Input'
 
-export default function Home() {
+export default function SignIn() {
   return (
-   <Flex >
-    
+   <Flex
+     w="100vw" 
+     h="100vh" 
+     align="center" 
+     justify="center"
+    >
+      <Flex 
+        as= "form" 
+        w="100%"
+        maxW={360}
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+        flexDir="column"
+      >
+        <Stack spacing={4}>
+          <Input name="email" label="Email" type="email" />
+          <Input name="password" label="Senha" type="password" />
+        </Stack>
+
+        <Button type="submit" mt="6" colorScheme={'pink'}>Entrar</Button>
+      </Flex>
    </Flex>
   )
 }
