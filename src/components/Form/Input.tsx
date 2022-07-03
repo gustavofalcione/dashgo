cc8c82d841d53caa5773eb749e788b383400ae2c
gustavo falcione <falcionegustavo@gmail.com>
2022-07-03 17:53:18 -0300

    Configurando validaçao dos dados com yup e react form

--- src/components/Form/Input.tsx
+++ src/components/Form/Input.tsx
@@ -1,27 +1,45 @@
-import { FormControl, FormLabel, Input as ChakraInput, InputProps as ChakraInputProps } from "@chakra-ui/react";
+import {
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Input as ChakraInput,
+  InputProps as ChakraInputProps,
+} from "@chakra-ui/react";
+import { forwardRef, ForwardRefRenderFunction } from "react";
+import { FieldError } from "react-hook-form";
 
 interface InputProps extends ChakraInputProps {
   name: string;
   label?: string;
-} 
+  error?: FieldError;
+}
 
-export function Input({ name, label, ...rest }: InputProps) {
+const InputBase: ForwardRefRenderFunction<HTMLInputElement, InputProps> = (
+  { name, label, error = null, ...rest },
+  ref
+) => {
   return (
-    <FormControl>
-      { !!label && <FormLabel htmlFor={name}>{label}</FormLabel> }
+    <FormControl isInvalid={!!error}>
+      {/* !! transforma o valor em booleano, se tiver algum valor será true, senão false */}
+      {!!label && <FormLabel htmlFor={name}>{label}</FormLabel>}
 
-      <ChakraInput 
+      <ChakraInput
         name={name}
         id={name}
         focusBorderColor="pink.500"
         bg="gray.900"
         variant="filled"
         _hover={{
-          bgColor: 'gray.900'
+          bgColor: "gray.900",
         }}
         size="lg"
+        ref={ref}
         {...rest}
       />
+
+      {!!error && <FormErrorMessage>{error.message}</FormErrorMessage>}
     </FormControl>
   );
-}
+};
+
+export const Input = forwardRef(InputBase);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,30 +1,74 @@
-import { Flex, Button, Stack } from '@chakra-ui/react'
-import { Input } from '../components/Form/Input'
+import { Flex, Button, Stack } from "@chakra-ui/react";
+import { SubmitHandler, useForm } from "react-hook-form";
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup'
+import { Input } from "../components/Form/Input";
+
+type SignInFormData = {
+  email: string;
+  password: string;
+};
+
+const signInFormSchema = yup.object().shape({
+  email: yup.string().required('E-mail obrigatório').email('E-mail deve ter um formato válido'),
+  password: yup.string().required('Senha obrigatória')
+})
 
 export default function SignIn() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<SignInFormData>({
+    resolver: yupResolver(signInFormSchema)
+  });
+
+  console.log(errors);
+
+  const handleSignIn: SubmitHandler<SignInFormData> = async (values) => {
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    console.log(values);
+  };
+
   return (
-   <Flex
-     w="100vw" 
-     h="100vh" 
-     align="center" 
-     justify="center"
-    >
-      <Flex 
-        as= "form" 
+    <Flex w="100vw" h="100vh" align="center" justify="center">
+      <Flex
+        as="form"
         w="100%"
         maxW={360}
         bg="gray.800"
         p="8"
         borderRadius={8}
         flexDir="column"
+        onSubmit={handleSubmit(handleSignIn)}
       >
-        <Stack spacing={4}>
-          <Input name="email" label="Email" type="email" />
-          <Input name="password" label="Senha" type="password" />
+        <Stack spacing="4">
+          <Input
+            name="email"
+            type="email"
+            label="Email"
+            error={errors.email}
+            {...register("email")}
+          />
+          <Input
+            name="password"
+            label="Senha"
+            type="password"
+            error={errors.password}
+            {...register("password")}
+
+          />
         </Stack>
 
-        <Button type="submit" mt="6" colorScheme={'pink'}>Entrar</Button>
+        <Button
+          type="submit"
+          mt="6"
+          colorScheme={"pink"}
+          isLoading={isSubmitting}
+        >
+          Entrar
+        </Button>
       </Flex>
-   </Flex>
-  )
+    </Flex>
+  );
 }
